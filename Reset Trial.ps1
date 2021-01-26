5561b24e74d90985f5e3b48c13353f09c6295fc3
Dj Stalin <52607377+xhz8s@users.noreply.github.com>
2021-01-26 21:24:09 +0100

    Delete Reset Trial.ps1

--- Reset Trial.ps1
+++
@@ -1,5 +0,0 @@
-& "C:\Program Files (x86)\F-Secure\Freedome\Freedome\1\uninstall.exe" /run /exit /SilentMode
-Set-Itemproperty -path "HKLM:\SOFTWARE\Microsoft\Cryptography" -Name 'MachineGuid' -value (New-Guid).ToString()
-wget "https://download.sp.f-secure.com/freedome/installer/2/Freedome.exe" -outfile ((Get-Location).ToString() + "/Freedome.exe")
-& ((Get-Location).ToString() + "/Freedome.exe")
-Remove-Item ((Get-Location).ToString() + "/Freedome.exe")
